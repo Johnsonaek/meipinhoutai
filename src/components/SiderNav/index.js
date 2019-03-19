a4a495e99289adde54c06f2b7f1af1f0d8a44879
panxianghui <320516548@qq.com>
2019-03-19 17:00:10 +0800

    初步改动

--- src/components/SiderNav/index.js
+++ src/components/SiderNav/index.js
@@ -3,31 +3,39 @@ import CustomMenu from "../CustomMenu/index";
 
 const menus = [
   {
-    title: '首页',
+    title: '合伙人管理',
     icon: 'home',
-    key: '/home'
+    key: '/home',
+    subs: [
+      {key: '/home/general/button', title: '合伙人列表', icon: '',},
+
+    ]
   },
   {
-    title: '基本组件',
+    title: '数据管理',
     icon: 'laptop',
     key: '/home/general',
     subs: [
-      {key: '/home/general/button', title: '按钮', icon: '',},
-      {key: '/home/general/icon', title: '图标', icon: '',},
+      {key: '/home/general/button', title: '实时概况', icon: '',},
+      {key: '/home/general/icon', title: '核心指标', icon: '',},
+      {key: '/home/general/button', title: '流量看板', icon: '',},
+      {key: '/home/general/icon', title: '客户看板', icon: '',},
+      {key: '/home/general/icon', title: '评价体系', icon: '',},
     ]
   },
   {
-    title: '导航组件',
+    title: '会员',
     icon: 'bars',
     key: '/home/navigation',
     subs: [
-      {key: '/home/navigation/dropdown', title: '下拉菜单', icon: ''},
-      {key: '/home/navigation/menu', title: '导航菜单', icon: ''},
-      {key: '/home/navigation/steps', title: '步骤条', icon: ''},
+      {key: '/home/navigation/dropdown', title: '客户管理', icon: ''},
+      {key: '/home/navigation/menu', title: '客户查询', icon: ''},
+      {key: '/home/navigation/steps', title: '标签管理', icon: ''},
+      {key: '/home/navigation/steps', title: '客户运营', icon: ''},
     ]
   },
   {
-    title: '输入组件',
+    title: '商品管理',
     icon: 'edit',
     key: '/home/entry',
     subs: [
@@ -36,53 +44,49 @@ const menus = [
         title: '表单',
         icon: '',
         subs: [
-          {key: '/home/entry/form/basic-form', title: '基础表单', icon: ''},
-          {key: '/home/entry/form/step-form', title: '分步表单', icon: ''}
+          {key: '/home/entry/form/basic-form', title: '商品列表', icon: ''},
+          {key: '/home/entry/form/step-form', title: '上架审核', icon: ''}
         ]
       },
       {key: '/home/entry/upload', title: '上传', icon: ''},
     ]
   },
   {
-    title: '显示组件',
+    title: '店铺管理',
     icon: 'desktop',
     key: '/home/display',
     subs: [
-      {key: '/home/display/carousel', title: '轮播图', icon: ''},
-      {key: '/home/display/collapse', title: '折叠面板', icon: ''},
-      {key: '/home/display/list', title: '列表', icon: ''},
-      {key: '/home/display/table', title: '表格', icon: ''},
-      {key: '/home/display/tabs', title: '标签页', icon: '',},
+      {key: '/home/display/carousel', title: '店铺列表', icon: ''},
+      {key: '/home/display/collapse', title: '开店备案', icon: ''},
+      {key: '/home/display/list', title: '开店审核', icon: ''},
     ]
   },
   {
-    title: '反馈组件',
+    title: '订单管理',
     icon: 'message',
     key: '/home/feedback',
     subs: [
-      {key: '/home/feedback/modal', title: '对话框', icon: '',},
-      {key: '/home/feedback/notification', title: '通知提醒框', icon: ''},
-      {key: '/home/feedback/spin', title: '加载中', icon: '',}
+      {key: '/home/feedback/modal', title: '订单处理', icon: '',},
+      {key: '/home/feedback/notification', title: '配送服务', icon: ''},
     ]
   },
   {
-    title: '其它',
+    title: '活动管理',
     icon: 'bulb',
     key: '/home/other',
     subs:[
-      {key: '/home/other/animation', title: '动画', icon: '',},
-      {key: '/home/other/gallery', title: '画廊', icon: '',},
-      {key:'/home/other/draft',title:'富文本',icon:''},
-      {key:'/home/other/chart',title:'图表',icon:''},
-      {key:'/home/other/loading',title:'加载动画',icon:''},
-      {key:'/home/other/404',title:'404',icon:''},
-      {key:'/home/other/springText',title:'弹性文字',icon:''},
+      {key: '/home/other/animation', title: '平台发起', icon: '',},
+      {key: '/home/other/gallery', title: '店铺发起', icon: '',},
     ]
   },
   {
-    title: '关于',
+    title: '运营支持',
     icon: 'info-circle-o',
-    key: '/home/about'
+    key: '/home/about',
+    subs:[
+      {key: '/home/other/animation', title: '宣传素材下载', icon: '',},
+      {key: '/home/other/gallery', title: '商学院', icon: '',},
+    ]
   }
 ]
 
